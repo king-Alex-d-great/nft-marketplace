9744395f00b6ed7d330fec0b592b4cb4e5d002fa
King-alex-d-great <ogubuikealex@gmail.com>
2022-01-17 19:53:31 +0100

    Add function to create Token

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -15,5 +15,13 @@ contract NFT is ERC721URIStorage {
     contractAddress = marketplaceAddress;
   }
 
-  
+  function createToken(string memory tokenURI) public returns (uint256) {
+    _tokenIds.increment();
+    uint256 newItemId = _tokenIds.current();
+
+    _mint(msg.sender, newItemId);
+    _setTokenURI(newItemId, tokenURI);
+    setApprovalForAll(contractAddress, true);
+    return newItemId;
+  }
 }
